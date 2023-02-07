a034aaaaaf084888560df685227595fa4ddf8a08
Bradley Bishop <bishopbm1@gmail.com>
2023-02-07 13:17:48 -0500

    Adding missing test file

---
+++ test/unit/test_tasks_key_decrypt.py
@@ -0,0 +1,18 @@
+from test.unit.st2_test_case import St2TestCase
+import mock
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'files'))
+from st2_task_base import St2TaskBase
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'tasks'))
+from key_get import AESKey
+
+
+class AESKeyTestCase(St2TestCase):
+    __test__ = True
+
+    def test_init(self):
+        task = AESKey()
+        self.assertIsInstance(task, St2TaskBase)

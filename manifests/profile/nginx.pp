6f7c82891364160e5981d000088c000988233e70
Rick Paxton <rick.paxton@encore.tech>
2024-05-20 13:28:58 -0400

    Pass nginx ssl params

--- manifests/profile/nginx.pp
+++ manifests/profile/nginx.pp
@@ -2,6 +2,10 @@
 #
 # @param manage_repo
 #    Set this to false when you have your own repository for nginx
+# @param ssl_ciphers
+#   Set the nginx SSL ciphers
+# @param ssl_protocols
+#   Set the nginx SSL protocols
 #
 # @example Basic Usage
 #  include st2::profile::nginx
@@ -12,10 +16,15 @@
 #  }
 #
 class st2::profile::nginx (
-  $manage_repo = $st2::nginx_manage_repo
+  Boolean  $manage_repo    = $st2::nginx_manage_repo,
+  String   $ssl_ciphers    = $st2::nginx_ssl_ciphers,
+  String   $ssl_protocols  = $st2::nginx_ssl_protocols,
 ) inherits st2 {
+  #
   class { 'nginx':
-    manage_repo => $manage_repo,
-    confd_purge => true,
+    confd_purge   => true,
+    manage_repo   => $manage_repo,
+    ssl_ciphers   => $ssl_ciphers,
+    ssl_protocols => $ssl_protocols,
   }
 }

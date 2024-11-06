c33f4268443a1b5a8c7d1caf1a79774d9fe5b4fb
Alex Chrystal <alex.chrystal@encore.tech>
2024-11-06 06:38:04 -0500

    testing

--- manifests/profile/mongodb.pp
+++ manifests/profile/mongodb.pp
@@ -101,7 +101,7 @@ class st2::profile::mongodb (
       #
       # To prevent this from running every time we've create a puppet fact
       # called $mongodb_auth_init that is set when
-      if !pick($facts['mongodb_auth_init'], false) {
+      if !$facts['mongodb_auth_init'] {
         # unfortinately there is no way to synchronously force a service restart
         # in Puppet, so we have to revert to exec... sorry
         include mongodb::params
